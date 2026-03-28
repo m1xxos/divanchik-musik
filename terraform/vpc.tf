8c19e44bbc7fe5de8274bf8f54682cc326653e3a
m1xxos <66390094+m1xxos@users.noreply.github.com>
2026-03-28 22:22:15 +0300

    feat:vpc

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -3,9 +3,26 @@ resource "yandex_vpc_network" "divanchik" {
   description = "Divanchik music k8s network"
 }
 
-resource "yandex_vpc_subnet" "divanchik-d" {
+resource "yandex_vpc_subnet" "divanchik_a" {
+  network_id     = yandex_vpc_network.divanchik.id
+  name           = "divanchik-a"
+  zone           = "ru-central1-a"
+  v4_cidr_blocks = ["10.5.0.0/16"]
+  description    = "Divanchick music zone-a k8s subnet"
+}
+
+resource "yandex_vpc_subnet" "divanchik_b" {
+  network_id     = yandex_vpc_network.divanchik.id
+  name           = "divanchik-b"
+  zone           = "ru-central1-a"
+  v4_cidr_blocks = ["10.6.0.0/16"]
+  description    = "Divanchick music zone-b k8s subnet"
+}
+
+resource "yandex_vpc_subnet" "divanchik_d" {
   network_id     = yandex_vpc_network.divanchik.id
   name           = "divanchik-d"
   zone           = "ru-central1-d"
-  v4_cidr_blocks = "192.168.1.0/24"
+  v4_cidr_blocks = ["10.7.0.0/16"]
+  description    = "Divanchick music zone-d k8s subnet"
 }
