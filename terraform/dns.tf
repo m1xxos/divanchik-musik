88d393a108e77f21d732bdb065af7c30ec8b4013
m1xxos <66390094+m1xxos@users.noreply.github.com>
2026-04-02 14:29:19 +0300

    reset 2

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,12 +1,9 @@
-data "yandex_alb_load_balancer" "gwin-gateway-gwin-divanchik-gateway" {
-  load_balancer_id = "ds7ms6lsr6e5rq8ta8tt"
-}
+
 
 resource "cloudflare_dns_record" "divanchik_record" {
   zone_id = var.cf_zone_id
   name    = "divanchik.m1xxos.online"
   type    = "A"
-  content = data.yandex_alb_load_balancer.gwin-gateway-gwin-divanchik-gateway.listener[0].endpoint[0].address[0].external_ipv4_address[0].address
   ttl     = 3600
 }
 
